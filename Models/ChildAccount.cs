018ef9a10dd78663113bbf9c610a3f249b41dbc1
Hank Stennes <hpstennes@gmail.com>
2021-05-16 15:31:49 -0500

    add new api stuff

---
+++ Models/ChildAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactDemo2.Models
+{
+    public partial class ChildrenAccount
+    {
+        public int ParentId { get; set; }
+        public float ChildId { get; set; }
+        public string AccountName { get; set; }
+        public int? FinancialId { get; set; }
+        public float? RelatedAccount { get; set; }
+
+        //public virtual ParentAccount ParentAccount { get; set; }
+    }
+}
