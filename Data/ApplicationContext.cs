8037896b3b93fd29a298bf7e3bf6209e8fd2b4ef
Hank Stennes <hpstennes@gmail.com>
2021-05-17 08:29:15 -0500

    Change some js files to jsx

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactDemo2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactDemo2.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+                : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
